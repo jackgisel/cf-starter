25917fd9bc661a5972bf8ce6eb5cb07f44893072
jackg <jackgisel@gmail.com>
2026-01-02 13:21:22 -0800

    more stuff

--- apps/data-service/src/workflows/destination-evaluation-workflow.ts
+++ apps/data-service/src/workflows/destination-evaluation-workflow.ts
@@ -2,9 +2,15 @@ import { WorkflowStep, WorkflowEntrypoint, WorkflowEvent } from "cloudflare:work
 
 import { collectDestinationInfo } from "@/helpers/browser-render";
 import { aiDestinationChecker } from "@/helpers/ai-destination-checker";
+import { addEvaluation } from "@repo/data-ops/queries/evaluations";
+import { initDatabase } from "@repo/data-ops/database";
+
 
 export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, DestinationStatusEvaluationParams> {
+
     async run(event: Readonly<WorkflowEvent<DestinationStatusEvaluationParams>>, step: WorkflowStep) {
+        initDatabase(this.env.db)
+        
         const collectedData = await step.do("Collect rendered destination page data", async () => {
             return collectDestinationInfo(this.env, event.payload.destinationUrl);
         });
@@ -18,6 +24,27 @@ export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, Desti
             return await aiDestinationChecker(this.env, collectedData.bodyText)
         })
 
-        return aiStatus
+        const evaluationId = await step.do('Save evaluation in database', async () => {
+			return await addEvaluation({
+				linkId: event.payload.linkId,
+				status: aiStatus.status,
+				reason: aiStatus.statusReason,
+				accountId: event.payload.accountId,
+				destinationUrl: event.payload.destinationUrl,
+			});
+		});
+
+        await step.do('Backup evaluation HTML to R2', async () => {
+            const accountId = event.payload.accountId;
+            const r2PathHtml = `evaluations/${accountId}/html/${evaluationId}`;
+            const r2PathBodyText = `evaluations/${accountId}/body-text/${evaluationId}`;
+            const r2PathScreenshot = `evaluations/${accountId}/screenshot/${evaluationId}`;
+            await this.env.r2.put(r2PathHtml, collectedData.html)
+            await this.env.r2.put(r2PathBodyText, collectedData.bodyText)
+
+            const base64Screenshot = collectedData.screenshotDataUrl.replace(/^data:image\/png;base64,/, '');
+            const screenshotBuffer = Buffer.from(base64Screenshot, 'base64');
+            await this.env.r2.put(r2PathScreenshot, screenshotBuffer)
+        });
     }
 }
--- apps/user-application/worker/trpc/routers/evaluations.ts
+++ apps/user-application/worker/trpc/routers/evaluations.ts
@@ -1,11 +1,11 @@
 import { t } from "@/worker/trpc/trpc-instance";
-
+import { getNotAvailableEvaluations, getEvaluations } from "@repo/data-ops/queries/evaluations";
 import { z } from "zod";
-import { EVALUATION_ISSUES, EVALUATIONS } from "./dummy-data";
+
 
 export const evaluationsTrpcRoutes = t.router({
-  problematicDestinations: t.procedure.query(async ({}) => {
-    return EVALUATION_ISSUES;
+  problematicDestinations: t.procedure.query(async ({ ctx }) => {
+    return await getNotAvailableEvaluations(ctx.userInfo.userId);
   }),
   recentEvaluations: t.procedure
     .input(
@@ -15,8 +15,8 @@ export const evaluationsTrpcRoutes = t.router({
         })
         .optional(),
     )
-    .query(async ({}) => {
-      const evaluations = EVALUATIONS;
+    .query(async ({ ctx }) => {
+      const evaluations = await getEvaluations(ctx.userInfo.userId);
 
       const oldestCreatedAt =
         evaluations.length > 0
